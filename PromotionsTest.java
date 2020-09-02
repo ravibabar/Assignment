665757bd382db24d7d154ab3b6d04ac17239a614
ravibabar <45914125+ravibabar@users.noreply.github.com>
2020-09-02 15:18:12 +0530

    Add files via upload

--- PromotionsTest.java
+++ PromotionsTest.java
@@ -7,24 +7,21 @@ import com.promos.discounts.MultipleUnitsPromotion;
 import junit.framework.Assert;
 
 public class PromotionsTest {
-	
-	
+
 	@Test
-	public void testPromotionForMultipleUnits1(){
-		MultipleUnitsPromotion promoEngine=new MultipleUnitsPromotion('A');
+	public void testPromotionForMultipleUnits1() {
+		MultipleUnitsPromotion promoEngine = new MultipleUnitsPromotion('A');
 		promoEngine.setItems(5);
-		
+
 		Assert.assertEquals(230.0, promoEngine.calculateDiscount());
 	}
-	
-	
+
 	@Test
-	public void testPromotionForMultipleUnits2(){
-		MultipleUnitsPromotion promoEngine=new MultipleUnitsPromotion('B');
+	public void testPromotionForMultipleUnits2() {
+		MultipleUnitsPromotion promoEngine = new MultipleUnitsPromotion('B');
 		promoEngine.setItems(5);
-		
+
 		Assert.assertEquals(120.0, promoEngine.calculateDiscount());
 	}
-	
 
 }
