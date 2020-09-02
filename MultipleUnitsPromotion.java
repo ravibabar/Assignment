bc62dcf2ae8e4101bc694373960db1f614e91fc9
ravibabar <45914125+ravibabar@users.noreply.github.com>
2020-09-02 15:23:07 +0530

    Add files via upload
    formatting and change in test

--- MultipleUnitsPromotion.java
+++ MultipleUnitsPromotion.java
@@ -1,76 +1,36 @@
 package com.promos.discounts;
 
-public class MultipleUnitsPromotion implements Promotion{
-	
+public class MultipleUnitsPromotion implements Promotion {
+
 	int noOfUnits;
 	char unit;
 	double price;
 	double groupPrice;
-	
 	int items;
-	private double dis;
-	
-	public MultipleUnitsPromotion( char unit) {
+
+	public MultipleUnitsPromotion(char unit, int items) {
 		this.unit = unit;
-		this.price = UnitsPricesHelper.getPrice(new Character(unit)).intValue();
-		this.noOfUnits=UnitsPricesHelper.getnoOfUnitsForUnits(unit);
-		this.groupPrice=UnitsPricesHelper.getPriceForNoOfUnits(unit);
-	}
-	public int getNoOfUnits() {
-		return noOfUnits;
-	}
-
-	public void setNoOfUnits(int noOfUnits) {
-		this.noOfUnits = noOfUnits;
-	}
-
-	public char getUnit() {
-		return unit;
-	}
-
-	public void setUnit(char unit) {
-		this.unit = unit;
-	}
-
-	public double getPrice() {
-		return price;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
-	}
-	
-	public int getItems() {
-		return items;
-	}
-
-	public void setItems(int items) {
 		this.items = items;
+		this.price = UnitsPricesHelper.getPrice(new Character(unit)).intValue();
+		this.noOfUnits = UnitsPricesHelper.getnoOfUnitsForUnits(unit);
+		this.groupPrice = UnitsPricesHelper.getPriceForNoOfUnits(unit);
 	}
 
 	@Override
-	public double calculateDiscount(){
+	public double calculateDiscount() {
 		double dis = 0;
-		double totPrice=0;
+		double totPrice = 0;
 
-		int rem=items%noOfUnits;
-		totPrice=price*rem; 
-		totPrice+=(items/noOfUnits)*groupPrice;
+		int rem = items % noOfUnits;
+		totPrice = price * rem;
+		totPrice += (items / noOfUnits) * groupPrice;
 
-		dis=(items*price)-totPrice;
-		this.setDis(dis);
-		
+		// calculated for future purpose if required to check promotion
+		// based on discount
+		dis = (items * price) - totPrice;
+
 		return totPrice;
-		
-	}
 
-	private void setDis(double dis) {
-       this.dis=dis;		
-	}
-	
-
-	public double getDis(){
-       return this.dis;	
 	}
 
 }
