a6e90d27dd0aefc54d719e68f6628ffb632316d8
ravibabar <babarravindra@gmail.com>
2020-09-03 16:00:25 +0530

    refactoring

--- src/com/promos/engine/PromotionEngine.java
+++ src/com/promos/engine/PromotionEngine.java
@@ -13,50 +13,48 @@ public class PromotionEngine {
 
 		double totPrice = 0.0;
 		Promotion p = null;
+		boolean flag = false;
 
-		int cValue = 0;
-		boolean flag=false;
-		
 		for (Map.Entry<Character, Integer> entry : hs.entrySet()) {
 
 			char c = entry.getKey();
 			int v = entry.getValue();
-			
 
 			switch (c) {
 
 			case 'A':
-				p = new MultipleItemsPromotion(c, v); // needs to change to have
-														// loose coupling
+				p = new MultipleItemsPromotion(c, v); // needs to change to have loose coupling
 				totPrice += p.calculateDiscount();
 				break;
 			case 'B':
-				p = new MultipleItemsPromotion(c, v); // needs to change to have
-														// loose coupling
+				p = new MultipleItemsPromotion(c, v); // needs to change to have loose coupling
 				totPrice += p.calculateDiscount();
 				break;
 			case 'C':
-				if(flag) break;
-				if(!hs.containsKey('D')){
+				if (flag)
+					break;
+				if (!hs.containsKey('D')) {
 					p = new MultipleItemsPromotion(c, v);
 					totPrice += p.calculateDiscount();
-				}else{
+				} else {
 					p = new TwoItemsPromotion(v, hs.get('D'));
 					totPrice += p.calculateDiscount();
-					flag=true;
+					flag = true;
 				}
 				break;
 
-			case 'D':if(flag) break;
-				if(!hs.containsKey('C')){
-				p = new MultipleItemsPromotion(c, v);
-				totPrice += p.calculateDiscount();
-				}else{
+			case 'D':
+				if (flag)
+					break;
+				if (!hs.containsKey('C')) {
+					p = new MultipleItemsPromotion(c, v);
+					totPrice += p.calculateDiscount();
+				} else {
 					p = new TwoItemsPromotion(v, hs.get('C'));
 					totPrice += p.calculateDiscount();
-					flag=true;
+					flag = true;
 				}
-				
+
 			}
 
 		}
